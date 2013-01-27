41c8be2e888f87af1d9ad2c6d5d5004efb3b0ff4
Demetri Miller <demetri.miller@gmail.com>
2013-01-26 20:49:49 -0600

    Added demo project for prototyping overlay spec

---
+++ Demos/ViewerDemo/ViewerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ViewerDemo
+//
+//  Created by Demetri Miller on 1/26/13.
+//  Copyright (c) 2013 Demetri Miller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Demos/ViewerDemo/ViewerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewerDemo
+//
+//  Created by Demetri Miller on 1/26/13.
+//  Copyright (c) 2013 Demetri Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
