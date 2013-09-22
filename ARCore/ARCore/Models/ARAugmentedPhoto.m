74cbda37775b0de7d147ab986e13b7940156f0b3
Grayson Sharpe <grayson.g.sharpe@gmail.com>
2013-09-22 07:33:53 -0400

    Added code for adding EXIF data before augmenting images

--- ARCore/ARCore/Models/ARAugmentedPhoto.m
+++ ARCore/ARCore/Models/ARAugmentedPhoto.m
@@ -26,6 +26,9 @@
 #import "ARSite.h"
 #import "SBJSON.h"
 #import "ASIHTTPRequest+JSONAdditions.h"
+#import <ImageIO/ImageIO.h>
+#import <AVFoundation/AVFoundation.h>
+
 
 #define PHOTOS_DIRECTORY [@"~/Documents/Photos/" stringByExpandingTildeInPath]
 #define DEFAULT_JPEG_QUALITY 0.45
@@ -164,6 +167,8 @@
 
     _image = [[ARManager shared] rotateImage:_image byOrientationFlag:_image.imageOrientation];
 
+    [self addEXIFDataToImage];
+   
     ASIFormDataRequest * req = [self requestForProcessing];
     ASIFormDataRequest * __weak __req = req;
     
@@ -206,6 +211,98 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:NOTIF_AUGMENTED_PHOTO_UPDATED object: self];
     
     [req startAsynchronous];
+}
+
+- (void)addEXIFDataToImage{
+    NSData *jpeg = UIImageJPEGRepresentation(_image, 0.0);
+
+    CGImageSourceRef source;
+    source = CGImageSourceCreateWithData((__bridge CFDataRef)jpeg, NULL);
+
+    //get all the metadata in the image
+    NSDictionary *metadata = (__bridge NSDictionary *) CGImageSourceCopyPropertiesAtIndex(source,0,NULL);
+
+    //make the metadata dictionary mutable so we can add properties to it
+    NSMutableDictionary *metadataAsMutable = [metadata mutableCopy];
+
+    NSMutableDictionary *EXIFDictionary = [[metadataAsMutable objectForKey:(NSString *)kCGImagePropertyExifDictionary]mutableCopy];
+    NSMutableDictionary *GPSDictionary = [[metadataAsMutable objectForKey:(NSString *)kCGImagePropertyGPSDictionary]mutableCopy];
+    if(!EXIFDictionary) {
+        //if the image does not have an EXIF dictionary (not all images do), then create one for us to use
+        EXIFDictionary = [NSMutableDictionary dictionary];
+    }
+    if(!GPSDictionary) {
+        GPSDictionary = [NSMutableDictionary dictionary];
+    }
+
+    //Setup GPS dict
+    if ([[ARManager shared] locationEnabled]) {
+        [metadataAsMutable setObject:[self getGPSDictionaryForLocation:[[ARManager shared] deviceLocation]] forKey:(NSString *)kCGImagePropertyGPSDictionary];
+    }
+    
+    //add our modified EXIF data back into the image’s metadata
+    [metadataAsMutable setObject:EXIFDictionary forKey:(NSString *)kCGImagePropertyExifDictionary];
+}
+
+- (NSDictionary *)getGPSDictionaryForLocation:(CLLocation *)location {
+    NSMutableDictionary *gps = [NSMutableDictionary dictionary];
+
+    // GPS tag version
+    [gps setObject:@"2.2.0.0" forKey:(NSString *)kCGImagePropertyGPSVersion];
+
+    // Time and date must be provided as strings, not as an NSDate object
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"HH:mm:ss.SSSSSS"];
+    [formatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    [gps setObject:[formatter stringFromDate:location.timestamp] forKey:(NSString *)kCGImagePropertyGPSTimeStamp];
+    [formatter setDateFormat:@"yyyy:MM:dd"];
+    [gps setObject:[formatter stringFromDate:location.timestamp] forKey:(NSString *)kCGImagePropertyGPSDateStamp];
+
+    // Latitude
+    CGFloat latitude = location.coordinate.latitude;
+    if (latitude < 0) {
+        latitude = -latitude;
+        [gps setObject:@"S" forKey:(NSString *)kCGImagePropertyGPSLatitudeRef];
+    } else {
+        [gps setObject:@"N" forKey:(NSString *)kCGImagePropertyGPSLatitudeRef];
+    }
+    [gps setObject:[NSNumber numberWithFloat:latitude] forKey:(NSString *)kCGImagePropertyGPSLatitude];
+
+    // Longitude
+    CGFloat longitude = location.coordinate.longitude;
+    if (longitude < 0) {
+        longitude = -longitude;
+        [gps setObject:@"W" forKey:(NSString *)kCGImagePropertyGPSLongitudeRef];
+    } else {
+        [gps setObject:@"E" forKey:(NSString *)kCGImagePropertyGPSLongitudeRef];
+    }
+    [gps setObject:[NSNumber numberWithFloat:longitude] forKey:(NSString *)kCGImagePropertyGPSLongitude];
+
+    // Altitude
+    CGFloat altitude = location.altitude;
+    if (!isnan(altitude)){
+        if (altitude < 0) {
+            altitude = -altitude;
+            [gps setObject:@"1" forKey:(NSString *)kCGImagePropertyGPSAltitudeRef];
+        } else {
+            [gps setObject:@"0" forKey:(NSString *)kCGImagePropertyGPSAltitudeRef];
+        }
+        [gps setObject:[NSNumber numberWithFloat:altitude] forKey:(NSString *)kCGImagePropertyGPSAltitude];
+    }
+
+    // Speed, must be converted from m/s to km/h
+    if (location.speed >= 0){
+        [gps setObject:@"K" forKey:(NSString *)kCGImagePropertyGPSSpeedRef];
+        [gps setObject:[NSNumber numberWithFloat:location.speed*3.6] forKey:(NSString *)kCGImagePropertyGPSSpeed];
+    }
+
+    // Heading
+    if (location.course >= 0){
+        [gps setObject:@"T" forKey:(NSString *)kCGImagePropertyGPSTrackRef];
+        [gps setObject:[NSNumber numberWithFloat:location.course] forKey:(NSString *)kCGImagePropertyGPSTrack];
+    }
+
+    return gps;
 }
 
 - (void)processPostComplete:(ASIFormDataRequest*)req
